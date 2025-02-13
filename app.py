54b4c96b329a1b7a4253d198aea0679a5c5d4f5a
andrews <ansouza@grupopanvel.com.br>
2025-02-13 18:15:05 -0300

    feat: save schedule

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
-from flask import Flask, request
 import database_handler
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
 
 # controller
-
-@app.route('/hello', methods=['GET'])
-def hello_world():
-    return 'Hello World!'
-
 @app.route('/schedule', methods=['POST'])
 def hello_name():
     data = request.json
-    schedule = {
-        "user_id": data["user_id"],
-        "schedule": data["schedule"]
-    }
-    print(schedule)
-    return database_handler.insert_user_schedule(schedule)
-
-
-
+    return database_handler.insert_user_schedule(data['schedule'])
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- database_handler.py
+++ database_handler.py
@@ -1,32 +1,29 @@
+import uuid
+from datetime import datetime
+
 import boto3
 from config.environment import Environment
-from botocore.exceptions import ParamValidationError
 
 dynamo_db = boto3.client(
     "dynamodb",
     aws_access_key_id=Environment().AWS_ACCESS_KEY_ID,
     aws_secret_access_key=Environment().AWS_SECRET_ACCESS_KEY,
-    region_name="us-east-2"
+    region_name="sa-east-1"
 )
 
-table_name = "userSchedule"
+TABLE_NAME = "ai-service-schedule"
 
 
 def insert_user_schedule(schedule):
-    print(schedule)
-    try:
-        response = dynamo_db.put_item(
-            TableName=table_name,
-            Item={
-                "id": {"N": str(schedule["user_id"])},  # "N" para Number
-                "user_id": {"N": str(schedule["user_id"])},  # "N" para Number
-                "schedule": {"S": schedule["schedule"]}   # "S" para String
-            }
-        )
-        return response
-    except ParamValidationError as e:
-        print(f"Erro de validação: {e}")
-        return {"error": str(e)} # Retorna um dicionário com o erro para tratamento
-    except Exception as e: # Captura outros erros
-        print(f"Erro ao inserir item: {e}")
-        return {"error": str(e)}
+    item = {
+        "id": {"S": str(uuid.uuid4())},  # Gera um UUID e armazena como string
+        "date":{"S": datetime.now().strftime("%Y-%m-%d")},  # Armazena a data atual como string
+        "schedule": {"S": schedule}  # Mantém o schedule como string
+    }
+
+    response = dynamo_db.put_item(
+        TableName=TABLE_NAME,
+        Item=item
+    )
+
+    return response
